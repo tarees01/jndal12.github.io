7cdcdee38f4b9d39a05860ec09a68c7f13fb4dcd
Juan <navaju01@luther.edu>
2014-11-02 15:30:06 -0600

    arwet

--- wikisearch.js
+++ wikisearch.js
@@ -1,24 +1,29 @@
 (function( $ ) {
 $.fn.wikiSearch = function(opts) {
-Util.options = opts;
-var labelTxt = Util.readOpt("label", "Search Wikipedia");
-var lang = Util.readOpt("lang", "en");
-var label = $("<label>").html(labelTxt);
-var input = $("<input>");
-var onLink = Util.readOpt("callback", function(lemma, language) { location.href = "http://" + language + ".wikipedia.org/wiki/" + lemma; })
-var resultList = $("<ul>");
-var listResults = function(results) {
-resultList.html("");
-if(results["1"] && results["1"].length > 0) {
-$.each(results[1], function(i, hit) {
-var item = $("<li>").append($("<a>").append(hit).click(function() { onLink(hit, lang); }).css({color: "blue", cursor: "pointer"}) );
-resultList.append(item);
-});
-} else {
-resultList.html("<li>no hits</li>");
-}
+
+	Util.options = opts;
+	var labelTxt = Util.readOpt("label", "Search Wikipedia");
+	var lang = Util.readOpt("lang", "en");
+	var label = $("<label>").html(labelTxt);
+	var input = $("<input>");
+	var onLink = Util.readOpt("callback", function(lemma, language) { location.href = "http://" + language + ".wikipedia.org/wiki/" + lemma; })
+	var resultList = $("<ul>");
+	var listResults = function(results) {
+		resultList.html("");
+		if(results["1"] && results["1"].length > 0) {
+			$.each(results[1], function(i, hit) {
+				var item = $("<li>").append($("<a>").append(hit).click(function() { onLink(hit, lang); }).css({color: "blue", cursor: "pointer"}) );
+				resultList.append(item);
+			});
+			
+			
+		} else {
+			resultList.html("<li>no hits</li>");
+		}
 };
-var flagLink = $("<a>").append($("<img>").attr("src", "img/" + lang + ".png")).css({"float": "right", cursor: "pointer"});
+
+/*var flagLink = $("<a>").append($("<img>").attr("src", "img/" + lang + ".png")).css({"float": "right", cursor: "pointer"});
+
 var flagPicker = $("<div>").css({"float": "right", display: "none"});
 flagLink.click(function(e) { flagPicker.show(); $(this).hide();});
 $.each(["nl", "en", "fr", "de"], function(i, language) {
@@ -31,23 +36,23 @@ flagPicker.hide();
 flagLink.show();
 });
 flagPicker.append(flagSetter);
-});
+});*/
 var sendSearch = function(q) {
-$.ajax("https://" + lang + ".wikipedia.org/w/api.php", {
-data: {action: "opensearch", search: q, limit: 10, namespace:0, format: "json"},
-dataType: "jsonp",
-success: listResults
-});
+	$.ajax("https://" + lang + ".wikipedia.org/w/api.php", {
+		data: {action: "opensearch", search: q, limit: 10, namespace:0, format: "json"},
+		dataType: "jsonp",
+		success: listResults
+	});
 };
 this.append(flagPicker).append(flagLink).append(label).append("<br>").append(input).append(resultList);
 input.on("change", function(e) {
-if(this.value.length > 0) {
-sendSearch(this.value);
-}
+	if(this.value.length > 0) {
+		sendSearch(this.value);
+	}
 });
 input.on("keypress", function(e) {
-if($.browser.msie && e.keyCode == 13 && this.value.length > 0) {
-sendSearch(this.value);
-}
+	if($.browser.msie && e.keyCode == 13 && this.value.length > 0) {
+		sendSearch(this.value);
+	}
 });
 }})(jQuery);
