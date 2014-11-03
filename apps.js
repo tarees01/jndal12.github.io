2281e06e5df7c321710e23a18d2224c2080959d1
Juan <navaju01@luther.edu>
2014-11-03 17:35:17 -0600

    ny

--- apps.js
+++ apps.js
@@ -5,7 +5,7 @@ $('#content').html(contentHtml);
 function startSearch(inputText) {
 $.ajax({
 //url: 'https://ru.wikipedia.org/w/api.php?format=json&action=mobileview&redirects&prop=sections|normalizedtitle&sectionprop=toclevel|line|index&page=%D0%A4%D1%83%D0%BD%D0%BA%D1%86%D0%B8%D1%8F%20(%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5)',
-url: 'https://ru.wikipedia.org/w/api.php?format=json&action=parse&redirects&prop=text&mobileformat=html&page=' + inputText + '&section=0',
+url: 'https://en.wikipedia.org/w/api.php?format=json&action=parse&redirects&prop=text&mobileformat=html&page=' + inputText + '&section=0',
 //url: 'http://en.wikipedia.org/w/api.php?format=json&action=query&titles=Main%20Page&prop=revisions&rvprop=content&callback=?',
 dataType: 'jsonp'
 }).done(function (data) {
