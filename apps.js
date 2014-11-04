92f102b4bf0919cc241d23747e8b6df1b84cc78d
Juan <navaju01@luther.edu>
2014-11-04 01:52:51 -0600

    good?

--- apps.js
+++ apps.js
@@ -43,13 +43,63 @@ showResult(data.parse.text['*']);
 }
 });
 }
+
+var searchWikipedia = function(currentTerm){
+var url = "http://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=";
+$.ajax({
+url: url + currentTerm,
+type: 'GET',
+dataType: 'jsonp',
+error: function(data){
+console.log("We got problems");
+console.log(data.status);
+},
+success: function(data){
+console.log("WooHoo!");
+//Check the browser console to see the returned data
+console.log(data);
+//Use jQuery to insert the search term into the appropriate DOM element
+//The data we want is the first item in the returned JSON, hence value "0"
+$("#searchTerm").html(data[0]);
+//The data we want is the second item in the returned JSON, hence value "1"
+//Create a var to save the array of search results
+var searchResults = data[1];
+
+/*var $ul = $('<ol></ol>');
+for (var i = 0; i < data.query.search.length; i++) {
+var item = data.query.search[i];
+var $li = $('<li></li>');
+
+$li.append('<h3><a href="#" class="snippet-item">' + item.title +  '</a></h3>');
+$li.append(item.snippet);
+$ul.append($li);*/
+//Loop through the array of results
+for (var i = 0; i < searchResults.length; i++){
+//Use 'replace' and a regular expression to substitue white space with '_' character
+var resultTerms = searchResults[i].replace(/\s/g, '_');
+var curURL = 'http://en.wikipedia.org/wiki/' + item;
+//Use jQuery's append() function to add the searchResults to the DOM
+//The argument for the append function will be a string of HTML
+$("#resultsTarget").append(
+"<p class='wikiResults'>" +
+"<a href=" + curURL + ">" +
+searchResults[i] +
+"</a>" +
+"</p>"
+);
+}
+}
+});
+};
 $('#search-button').on('click', function () {
 var inputText = $.trim($('#query').val());
 startSearch(inputText);
 });
 $(document).on('click', '.snippet-item', function (event) {
-event.preventDefault();
-var text = $(this).text();
-startSearch(text);
+//event.preventDefault();
+var newSearchTerm = $("#query").val();
+searchWikipedia(newSearchTerm);
+//var text = $(this).text();
+//startSearch(text);
 });
 });
