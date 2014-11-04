1854b0dac11424bc263c7d29af6cad0183dc4237
Juan <navaju01@luther.edu>
2014-11-04 01:11:59 -0600

    aewqf

--- apps.js
+++ apps.js
@@ -43,7 +43,7 @@ showResult(data.parse.text['*']);
 }
 });
 }
-$('#seach-button').on('click', function () {
+$('#s-form button').on('click', function () {
 var inputText = $.trim($('#s-form input').val());
 startSearch(inputText);
 });
